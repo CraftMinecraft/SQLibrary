8021bedae6ce927e20c1203a9f631253d0b4e1bf
Robin Lambertz <robinlambertz.dev@gmail.com>
2013-02-26 22:05:11 +0000

    Update SQLibrary to work with BungeeCord.
    Updated for bungeecord support.

--- lib/PatPeter/SQLibrary/SQLibrary.java
+++ lib/PatPeter/SQLibrary/SQLibrary.java
@@ -1,39 +1,40 @@
 /*
  * Date Created: 2012-03-11 14:20
  * @author <a href="http://forums.bukkit.org/members/jertocvil.18552/">jertocvil</a>
+ * @author roblabla
  */
 package lib.PatPeter.SQLibrary;
  
 import java.util.logging.Level;
 import java.util.logging.Logger;
  
-import org.bukkit.plugin.java.JavaPlugin;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.ProxyServer;
 
 /**
  * Class for running the library as a plugin.<br>
  * Date Created: 2012-03-11 14:20.
  * 
  * @author Nicholas Solin, a.k.a. PatPeter
+ * @author Robin Lambertz, a.k.a. Roblabla
  */
-public class SQLibrary extends JavaPlugin {
+public class SQLibrary extends Plugin {
 	/**
 	 * Logger for the Minecraft server.
 	 */
-	public static final Logger logger = Logger.getLogger("Minecraft");
+	public static final Logger logger = ProxyServer.getInstance().getLogger();
 	
 	/**
 	 * onEnable() method for the plugin.
 	 */
-	@Override
 	public void onEnable() {
-	    SQLibrary.logger.log(Level.INFO, "SQLibrary loaded.");
+	    SQLibrary.logger.log(Level.INFO, "[SQLibrary] SQLibrary loaded.");
 	}
 	
 	/**
 	 * onDisable method for the plugin.
 	 */
-	@Override
 	public void onDisable() {
-	    SQLibrary.logger.log(Level.INFO, "SQLibrary stopped.");
+	    SQLibrary.logger.log(Level.INFO, "[SQLibrary] SQLibrary stopped.");
 	}
 } 
